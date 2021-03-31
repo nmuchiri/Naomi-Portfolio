97f7c4ef1e0908c7378d1d8181e30042893084f5
nmuchiri <lonesomebounty@gmail.com>
2021-03-31 16:45:57 -0400

    edited app.js & index.js to render Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect} from 'react'
+
+
+const Home =()=>{
+
+
+return (
+    <div>
+    <h1>Welcome to my amazing portfolio</h1>
+    </div>
+
+)
+
+}
+
+export default Home
