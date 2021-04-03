b76321086f1219b57cc4f8e47b6ffd043600f719
nmuchiri <lonesomebounty@gmail.com>
2021-04-03 15:32:37 -0400

    added all icons and sized them on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,66 @@
-import { FaGithub, FaBootstrap, FaHtml5, FaJs, FaNode, FaPython, FaDatabase } from "react-icons/fa"
+import { FaGithub, FaBootstrap, FaHtml5, FaJs, FaNode, FaPython, FaDatabase, FaCss3 } from "react-icons/fa"
+import { DiMongodb } from "react-icons/di";
+import { SiFlask, SiPostgresql, SiPostman } from "react-icons/si";
 
 // import css
 import "./css/About.css";
 
 const About =()=>{
-
     return(
         <div className="container">
             <div>
                 <h1>This is all about me</h1>
             </div>
             <div>
-      <a href="https://drive.google.com/file/d/1toWlS6dtNsGZZ6w-53z2db8c9sEfg5nf/view?usp=sharing"
-        ><button>
-              My Resume
-              </button></a>
-      </div> 
-      <div>
-          <h1>My skills</h1>
-      </div>
-      <div className="col s4 m2 l2 icons">
-
-        <div>
-            <FaJs size="3em" ></FaJs>
-        </div>
-        <div>
-         <FaNode size="3em" ></FaNode>
-        </div>
-        <div>
-            <FaGithub size="3em" ></FaGithub>
-        </div>
-        <div>
-            <FaBootstrap size="3em" ></FaBootstrap>
-        </div>
-        <div>
-            <FaHtml5 size="3em"></FaHtml5>
-        </div>
-        <div>
-            <FaPython size="3em"></FaPython>
-        </div>
-        <div>
-        <FaDatabase size="3em" />
-        </div>
+                <a href="https://drive.google.com/file/d/1toWlS6dtNsGZZ6w-53z2db8c9sEfg5nf/view?usp=sharing"
+                    ><button>
+                    My Resume
+                    </button></a>
+            </div> 
+            <div>
+                <h1>My skills</h1>
+            </div>
+            <div className="row">
+            <div className="col-sm">
+                <h6>JavaScript</h6>
+                <FaJs size="2em" ></FaJs>
+            </div>
+            <div className="col-sm">
+                <FaNode size="2em" ></FaNode>
+            </div>
+            <div className="col-sm">
+                <FaGithub size="2em" ></FaGithub>
+            </div>
+            <div className="col-sm"> 
+                <FaBootstrap size="2em" ></FaBootstrap>
+            </div>
+            <div className="col-sm">
+                <FaHtml5 size="2em"></FaHtml5>
+            </div>
+            <div className="col-sm">
+                <FaPython size="2em"></FaPython>
+            </div>
+            <div className="col-sm">
+                <FaDatabase size="2em"></FaDatabase>
+            </div>
+            <div className="col-sm">
+                <FaCss3 size="2em"></FaCss3>
+            </div>
+            <div className="col-sm">
+                <DiMongodb size="2em"></DiMongodb>
+            </div>
+            <div className="col-sm">
+                <SiFlask size="2em"></SiFlask>
+            </div>
+            <div className="col-sm">
+                <SiPostgresql size= "2em"></SiPostgresql>
+            </div>
+            <div className="col-sm">
+                <SiPostman size= "2em"></SiPostman>
+            </div>
         
-      </div>
         </div>
+    </div>
     )
 
 }
